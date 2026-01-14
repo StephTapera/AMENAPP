6a0d79293a75f8a37fdccc82876e863a9e54f698
steph tapera <stephtapera@icloud.com>
2026-01-14 15:22:17 -0700

    Initial Commit

---
+++ AMENAPP/AMENAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMENAPPApp.swift
+//  AMENAPP
+//
+//  Created by Steph on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct AMENAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
